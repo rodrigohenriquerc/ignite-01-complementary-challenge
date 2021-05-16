1d646f54c03d6e57c2fcb133d5b09259948073eb
Rodrigo Carvalho <rhrc09@gmail.com>
2021-05-16 16:21:56 -0300

    refactor: move content into a separate component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenreTitle: string;
+  movies: Movie[];
+}
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenreTitle}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
