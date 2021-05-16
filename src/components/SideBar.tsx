30212bf0a0e6b7aed0a087691d75da085260ae2d
Rodrigo Carvalho <rhrc09@gmail.com>
2021-05-16 16:14:05 -0300

    refactor: move side bar into a separate component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+  isSelected: boolean;
+}
+
+interface SideBarProps {
+  genres: Genre[];
+  onSelectGenre: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onSelectGenre(genre.id)}
+            selected={genre.isSelected}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
